6611f613201adea8eb64793bcd671cffdf0b6155
MaxLearn <MaxLearn@users.noreply.github.com>
2021-07-21 15:21:23 +0700

    signgin reload

--- src/containers/header.js
+++ src/containers/header.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Header } from "../components";
-import * as ROUTES from "../constants/routes";
 import logo from "../logo.svg";
 
 export default function HeaderContainer({ children }) {
   return (
     <Header>
       <Header.Frame>
-        <Header.Logo to={ROUTES.HOME} alt="Netflix" src={logo} />
-        <Header.ButtonLink to={ROUTES.SIGN_IN}>Sign In</Header.ButtonLink>
+        <Header.Logo to="/home" alt="Netflix" src={logo} />
+        <Header.ButtonLink onClick={() => { window.location.href = "/signin"; }}>
+          Sign In
+        </Header.ButtonLink>
       </Header.Frame>
       {children}
     </Header>
